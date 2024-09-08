d4eb621019ce62d95623f33cd4c82ea76af1058f
cozycole <tritchcole@gmail.com>
2024-09-08 16:18:22 -0700

    add function for creating image name from a given input string (normally a title or name of video,creator or actor

---
+++ internal/models/helpers.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"regexp"
+	"strings"
+)
+
+func CreateImageName(input string, maxLength int) string {
+	formatted := strings.ToLower(input)
+	re := regexp.MustCompile(`[^a-zA-Z0-9\-\ ]+`)
+	formatted = re.ReplaceAllString(formatted, "")
+
+	words := strings.Split(formatted, " ")
+
+	// Rebuild string without exceeding maxLength,
+	// and avoid cutting off words
+	var result []string
+	currentLength := 0
+
+	for _, word := range words {
+		wordLength := len(word)
+
+		if currentLength+wordLength+1 > maxLength {
+			break
+		}
+
+		result = append(result, word)
+		// include the space between words in count
+		currentLength += wordLength + 1
+	}
+
+	return strings.Join(result, "-")
+}
---
+++ internal/models/helpers_test.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestCreateImageName(t *testing.T) {
+	maxLength := 40
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "SpecialCharacters",
+			input: "Cole's Comedy Sketch #awesome",
+			want:  "coles-comedy-sketch-awesome",
+		},
+		{
+			name:  "EmptyString",
+			input: "",
+			want:  "",
+		},
+		{
+			name:  "OverMaxLength",
+			input: "This is a test This is a test This a test",
+			want:  "this-is-a-test-this-is-a-test-this-a",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := CreateImageName(tt.input, maxLength)
+
+			if output != tt.want {
+				t.Errorf("got %q; want %q", output, tt.want)
+			}
+		})
+	}
+}
